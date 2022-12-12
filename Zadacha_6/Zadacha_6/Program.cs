543474e31800fb6114e84b3dcdbab50fbce3f862
Isaeva Lidiya <isaeva.reid@gmail.ru>
2022-12-13 02:12:39 +0500

    Решение 6 задачи

---
+++ Zadacha_6/Zadacha_6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите натуральное число: ");
+int i = int.Parse(Console.ReadLine());
+
+if (i%2 == 0)
+{
+    Console.WriteLine($"Число {i} чётное");
+}
+else
+{
+    Console.WriteLine($"Число {i} нечётное");
+}
