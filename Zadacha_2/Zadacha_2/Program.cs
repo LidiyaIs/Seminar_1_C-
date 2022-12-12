16f72251deb7201199c14a27c74cda07dd22207b
Isaeva Lidiya <isaeva.reid@gmail.ru>
2022-12-13 02:11:31 +0500

    Решение 2 задачи

---
+++ Zadacha_2/Zadacha_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"Число {a} большее, число {b} меньшее");
+}
+if (a < b)
+{
+    Console.WriteLine($"Число {b} большее, число {a} меньшее");
+}
+if (a == b)
+{
+    Console.WriteLine($"Числа {a} и {b} равны");
+}
