39ff66fe69341150d9149bec0dc7c2956e39c9e4
Isaeva Lidiya <isaeva.reid@gmail.ru>
2022-12-15 01:45:55 +0500

    Добавлено решение дз по семинару №2 - задачи 10, 13, 15

---
+++ Zadacha_13/Zadacha_13/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу,
+// которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int x = number;
+
+if (number < 100)
+{
+    Console.WriteLine($"У числа {number} нет третьей цифры");
+}
+else
+{
+    while (x > 1000)
+    {
+        x = x / 10;
+    }
+    //Console.WriteLine($"Число {number} {i}-значное");
+    x = x % 10;
+    Console.WriteLine($"У числа {number} третья цифра = {x}");
+}
