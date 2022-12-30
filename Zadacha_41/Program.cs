18527a8c07934dd4e82efd2c53b922904fab350a
Isaeva Lidiya <isaeva.reid@gmail.ru>
2022-12-31 03:09:12 +0500

    Добавлено решение дз по семинару №6 - задачи 41, 43

---
+++ Zadacha_41/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2 или 1, -7, 567, 89, 223-> 3
+
+Console.Write ("Введите количество чисел: ");
+int m = int.Parse(Console.ReadLine());
+
+int[] massive = GetArray(m);
+Console.WriteLine ($"Ваши числа: {GetArrayAsString(massive)}");
+
+Console.WriteLine ("Количество чисел больше 0 равно: "+ (Count (massive)));
+
+int[] GetArray (int length) // функция ввода массива
+{
+    int[] array = new int [length];
+    for (int i=0;i<array.Length;i++)
+    {
+        Console.Write($"Введите {i+1}-ое число:");
+        array[i]=int.Parse(Console.ReadLine());
+    }    
+    return array;
+}
+
+string GetArrayAsString (int[] array) //вывод массива в строку
+{
+    string s = string.Empty;
+    foreach (var item in array) s+=$"{item} ";
+    return s;
+}    
+
+int Count (int[] array)
+{
+    int kol = 0;
+    for (int i=0;i<array.Length;i++) 
+    {
+        if (array[i]>0) kol++;
+        
+    }
+    return kol;
+}
---
+++ Zadacha_43/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//значения b1, k1, b2 и k2 задаются пользователем. b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.WriteLine ("Введите данные для первой прямой");
+Console.Write ("угловой коэффициент: ");
+int k1 = int.Parse(Console.ReadLine()!);
+Console.Write ("свободный коэффициент: ");
+int b1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine ("Введите данные для второй прямой");
+Console.Write ("угловой коэффициент: ");
+int k2 = int.Parse(Console.ReadLine()!);
+Console.Write ("свободный коэффициент: ");
+int b2 = int.Parse(Console.ReadLine()!);
+SearchPoint (k1,b1,k2,b2);
+
+void SearchPoint(int m1,int n1,int m2,int n2)
+{
+    if (m1!= m2)
+    Console.WriteLine($"Точка пересечения двух прямых в координатах ({(n2 - n1)/(m1 - m2)};{m1 * ((n2 - n1) / (m1 - m2)) + n1})");
+    else Console.WriteLine("Прямые параллельны");
+}
